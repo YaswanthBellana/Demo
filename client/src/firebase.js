af21dc87258aac176ec642304d70e8431a08cac7
sivasathwik12 <145154019+sivasathwik12@users.noreply.github.com>
2023-09-16 00:34:31 +0530

    Add files via upload

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClN-OnZoAsoZKj4hP2zQ9u70GeGxpl1ZA",
+  authDomain: "twitter-clone-c6b94.firebaseapp.com",
+  projectId: "twitter-clone-c6b94",
+  storageBucket: "twitter-clone-c6b94.appspot.com",
+  messagingSenderId: "331290095218",
+  appId: "1:331290095218:web:d644df27fef63912f91ceb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
